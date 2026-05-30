1f53feeebf25885fe44df9276b02d1bc5b3baa84
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2026-05-31 02:41:34 +0530

    uh-hard coding prot , hehe

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Elysia } from "elysia";
 import { aiControllers } from "./modules/ai";
 
+const port = Number(process.env.PORT) || 3000;
+
 const app = new Elysia({ prefix: "/api" })
   .get("/", () => "Hello Elysia")
   .use(aiControllers)
-  .listen(3000);
+  .listen(port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
